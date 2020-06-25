77658cd3460c25208ebceb90b0a8d0e671039edc
Jeff <thejeffgiroux@gmail.com>
2020-06-25 01:05:14 -0700

    update standalone to 3nic

--- Azure/Standalone/main.tf
+++ Azure/Standalone/main.tf
@@ -2,10 +2,10 @@
 
 # Terraform Version Pinning
 terraform {
-    required_version = "~> 0.12.26"
-    required_providers {
-        azurerm = "~> 2.1.0"
-    }
+  required_version = "~> 0.12.26"
+  required_providers {
+    azurerm = "~> 2.1.0"
+  }
 }
 
 # Azure Provider
@@ -30,21 +30,4 @@ resource "azurerm_log_analytics_workspace" "law" {
   retention_in_days   = 300
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-}
-
-# Create the Storage Account
-resource "azurerm_storage_account" "mystorage" {
-  name                     = "${var.prefix}mystorage"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment             = var.environment
-    owner                   = var.owner
-    group                   = var.group
-    costcenter              = var.costcenter
-    application             = var.application
-  }
 }
--- Azure/Standalone/variables.tf
+++ Azure/Standalone/variables.tf
@@ -17,17 +17,14 @@ variable sp_client_secret {}
 variable sp_tenant_id {}
 variable prefix {}
 variable location {}
+variable storage_name {}
 
 # NETWORK
-variable cidr { default = "10.90.0.0/16" }
-variable "subnets" {
-  type = map
-  default = {
-    "subnet1" = "10.90.1.0/24"
-    "subnet2" = "10.90.2.0/24"
-    "subnet3" = "10.90.3.0/24"
-  }
-}
+variable vnet_rg {}
+variable vnet_name {}
+variable mgmtSubnet {}
+variable extSubnet {}
+variable intSubnet {}
 variable f5vm01mgmt { default = "10.90.1.4" }
 variable f5vm01ext { default = "10.90.2.4" }
 variable f5privatevip { default = "10.90.2.11" }
