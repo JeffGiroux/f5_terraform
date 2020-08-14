93b55f66a0a722fab78c18134f483d7fe63271f7
Jeff <thejeffgiroux@gmail.com>
2020-08-14 13:50:40 -0700

    update F5 automation toolchain to latest

--- GCP/HA_via_api/variables.tf
+++ GCP/HA_via_api/variables.tf
@@ -38,10 +38,10 @@ variable dns_server { default = "8.8.8.8" }
 variable dns_suffix {}
 variable ntp_server { default = "0.us.pool.ntp.org" }
 variable timezone { default = "UTC" }
-variable DO_URL { default = "https://github.com/F5Networks/f5-declarative-onboarding/releases/download/v1.13.0/f5-declarative-onboarding-1.13.0-5.noarch.rpm" }
-variable AS3_URL { default = "https://github.com/F5Networks/f5-appsvcs-extension/releases/download/v3.20.0/f5-appsvcs-3.20.0-3.noarch.rpm" }
-variable TS_URL { default = "https://github.com/F5Networks/f5-telemetry-streaming/releases/download/v1.12.0/f5-telemetry-1.12.0-3.noarch.rpm" }
-variable CF_URL { default = "https://github.com/F5Networks/f5-cloud-failover-extension/releases/download/v1.3.0/f5-cloud-failover-1.3.0-0.noarch.rpm" }
+variable DO_URL { default = "https://github.com/F5Networks/f5-declarative-onboarding/releases/download/v1.14.0/f5-declarative-onboarding-1.14.0-1.noarch.rpm" }
+variable AS3_URL { default = "https://github.com/F5Networks/f5-appsvcs-extension/releases/download/v3.21.0/f5-appsvcs-3.21.0-4.noarch.rpm" }
+variable TS_URL { default = "https://github.com/F5Networks/f5-telemetry-streaming/releases/download/v1.13.0/f5-telemetry-1.13.0-2.noarch.rpm" }
+variable CF_URL { default = "https://github.com/F5Networks/f5-cloud-failover-extension/releases/download/v1.4.0/f5-cloud-failover-1.4.0-0.noarch.rpm" }
 variable onboard_log { default = "/var/log/cloud/onboard.log" }
 
 # BIGIQ License Manager Setup
@@ -50,7 +50,7 @@ variable bigIqUsername { default = "admin" }
 variable bigIqLicenseType { default = "licensePool" }
 variable bigIqLicensePool { default = "myPool" }
 variable bigIqSkuKeyword1 { default = "key1" }
-variable bigIqSkuKeyword2 { default = "key1" }
+variable bigIqSkuKeyword2 { default = "key2" }
 variable bigIqUnitOfMeasure { default = "hourly" }
 variable bigIqHypervisor { default = "gce" }
 
