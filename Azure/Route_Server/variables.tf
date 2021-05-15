dd0d35e827017ab5c3472ecdf05a2ce72deebc8e
jeffgiroux <thejeffgiroux@gmail.com>
2021-05-14 17:25:59 -0700

    variable updates

--- Azure/Route_Server/variables.tf
+++ Azure/Route_Server/variables.tf
@@ -22,8 +22,21 @@ variable "keyName" {
   description = "instance key pair name"
 }
 variable "adminSrcAddr" {
-  type    = string
-  default = "0.0.0.0/0"
+  type        = string
+  description = "Allowed Admin source IP prefix"
+  default     = "0.0.0.0/0"
+}
+variable "availabilityZones" {
+  type        = list(any)
+  description = "If you want the VM placed in an Azure Availability Zone, and the Azure region you are deploying to supports it, specify the numbers of the existing Availability Zone you want to use."
+  default     = [1]
+}
+
+#BIG-IP info
+variable "instanceCountBigIp" {
+  type        = number
+  description = "Number of BIG-IP instances to deploy"
+  default     = 1
 }
 
 
