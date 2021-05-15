a1e8e4de3aef2b0865442e8f2323c987980a9864
jeffgiroux <thejeffgiroux@gmail.com>
2021-05-14 17:40:04 -0700

    add bigip

---
+++ Azure/Route_Server/bigip.tf
@@ -0,0 +1,28 @@
+module "bigip" {
+  count                      = var.instanceCountBigIp
+  source                     = "github.com/f5devcentral/terraform-azure-bigip-module"
+  prefix                     = format("%s-bigip-%s-%s", var.projectPrefix, count.index, random_id.buildSuffix.hex)
+  resource_group_name        = azurerm_resource_group.rg["hub"].name
+  mgmt_subnet_ids            = [{ "subnet_id" = data.azurerm_subnet.mgmtSubnetHub.id, "public_ip" = true, "private_ip_primary" = "" }]
+  mgmt_securitygroup_ids     = [module.nsg-mgmt["hub"].network_security_group_id]
+  external_subnet_ids        = [{ "subnet_id" = data.azurerm_subnet.externalSubnetHub.id, "public_ip" = true, "private_ip_primary" = "", "private_ip_secondary" = "" }]
+  external_securitygroup_ids = [module.nsg-external["hub"].network_security_group_id]
+  internal_subnet_ids        = [{ "subnet_id" = data.azurerm_subnet.internalSubnetHub.id, "public_ip" = false, "private_ip_primary" = "" }]
+  internal_securitygroup_ids = [module.nsg-internal["hub"].network_security_group_id]
+  availabilityZones          = var.availabilityZones
+  f5_ssh_publickey           = var.keyName
+}
+
+resource "null_resource" "clusterDO" {
+  count = var.instanceCountBigIp
+
+  provisioner "local-exec" {
+    command = "cat > DO_3nic-instance${count.index}.json <<EOL\n ${module.bigip[count.index].onboard_do}\nEOL"
+  }
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -rf DO_3nic-instance${count.index}.json"
+  }
+
+  depends_on = [module.bigip.onboard_do]
+}
