7ebd386bcfc69f31b055dd2d65911e0f69f59933
jeffgiroux <thejeffgiroux@gmail.com>
2021-08-13 12:09:44 -0700

    add rg to output

--- Azure/Infrastructure-only/outputs.tf
+++ Azure/Infrastructure-only/outputs.tf
@@ -1,5 +1,6 @@
 # Outputs
 
+output "resource_group" { value = azurerm_resource_group.main.name }
 output "vnet" { value = azurerm_virtual_network.main.name }
 output "mgmt_subnet" { value = azurerm_subnet.mgmt.address_prefix }
 output "external_subnet" { value = azurerm_subnet.external.address_prefix }
