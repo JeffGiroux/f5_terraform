caf9d2dc55f246b83ba878fc4727e560655ec020
jeffgiroux <thejeffgiroux@gmail.com>
2021-08-17 23:00:00 -0700

    storage naming

--- Azure/HA_via_api/main.tf
+++ Azure/HA_via_api/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_log_analytics_workspace" "law" {
 
 # Create the Storage Account
 resource "azurerm_storage_account" "main" {
-  name                     = format("%s-storage-%s", var.projectPrefix, random_id.buildSuffix.hex)
+  name                     = format("%sstorage%s", var.projectPrefix, random_id.buildSuffix.hex)
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
