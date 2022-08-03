84cb4ff7f0a189532cd28b28eee0d212e7686970
jeffgiroux <thejeffgiroux@gmail.com>
2022-08-03 11:34:00 -0700

    private ip outputs

--- Azure/Route_Server/outputs.tf
+++ Azure/Route_Server/outputs.tf
@@ -40,5 +40,5 @@ output "appPrivateIP" {
 }
 output "bigip-private-ips" {
   description = "The private ip address for BIG-IP"
-  value       = element(flatten(module.bigip.*.private_addresses.public_private.private_ip), 0)
+  value       = module.bigip.*.private_addresses
 }
